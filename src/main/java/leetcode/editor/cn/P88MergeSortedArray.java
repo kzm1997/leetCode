1216d04b467879967266a1df7531b98f1f5fd50b
kouzhengmin <1206960056@qq.com>
2020-12-30 15:23:22 +0800

    third

---
+++ src/main/java/leetcode/editor/cn/P88MergeSortedArray.java
@@ -0,0 +1,72 @@
+//给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。 
+//
+// 
+//
+// 说明： 
+//
+// 
+// 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。 
+// 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。 
+// 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入：
+//nums1 = [1,2,3,0,0,0], m = 3
+//nums2 = [2,5,6],       n = 3
+//
+//输出：[1,2,2,3,5,6] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// -10^9 <= nums1[i], nums2[i] <= 10^9 
+// nums1.length == m + n 
+// nums2.length == n 
+// 
+// Related Topics 数组 双指针 
+// 👍 725 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+
+//java:合并两个有序数组
+public class P88MergeSortedArray{
+    public static void main(String[] args){
+        Solution solution = new P88MergeSortedArray().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Make a copy of nums1.
+        int [] nums1_copy = new int[m];
+        System.arraycopy(nums1, 0, nums1_copy, 0, m);
+
+        // Two get pointers for nums1_copy and nums2.
+        int p1 = 0;
+        int p2 = 0;
+
+        // Set pointer for nums1
+        int p = 0;
+
+        // Compare elements from nums1_copy and nums2
+        // and add the smallest one into nums1.
+        while ((p1 < m) && (p2 < n))
+            nums1[p++] = (nums1_copy[p1] < nums2[p2]) ? nums1_copy[p1++] : nums2[p2++];
+
+        // if there are still elements to add
+        if (p1 < m)
+            System.arraycopy(nums1_copy, p1, nums1, p1 + p2, m + n - p1 - p2);
+        if (p2 < n)
+            System.arraycopy(nums2, p2, nums1, p1 + p2, m + n - p1 - p2);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
