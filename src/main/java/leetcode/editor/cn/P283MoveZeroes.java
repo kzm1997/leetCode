f16bf271b57ee8f0ae4a5fdb75c67b447d4ccbc2
kouzhengmin <1206960056@qq.com>
2021-01-05 09:13:07 +0800

    双指针

---
+++ src/main/java/leetcode/editor/cn/P283MoveZeroes.java
@@ -0,0 +1,44 @@
+//给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。 
+//
+// 示例: 
+//
+// 输入: [0,1,0,3,12]
+//输出: [1,3,12,0,0] 
+//
+// 说明: 
+//
+// 
+// 必须在原数组上操作，不能拷贝额外的数组。 
+// 尽量减少操作次数。 
+// 
+// Related Topics 数组 双指针 
+// 👍 904 👎 0
+
+package leetcode.editor.cn;
+//java:移动零
+public class P283MoveZeroes{
+    public static void main(String[] args){
+        Solution solution = new P283MoveZeroes().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums==null||nums.length==0){
+            return;
+        }
+        int j=0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]!=0){
+                nums[j++]=nums[i];
+            }
+
+        }
+        while (j<nums.length){
+            nums[j++]=0;
+        }
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
