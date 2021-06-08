2aa791f5a9d8694805e2875bd75d401b12bdaaec
kouzhengmin <1206960056@qq.com>
2021-06-08 13:19:04 +0800

    448

---
+++ src/main/java/leetcode/editor/cn/P448_FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,68 @@
+//给你一个含 n 个整数的数组 nums ，其中 nums[i] 在区间 [1, n] 内。请你找出所有在 [1, n] 范围内但没有出现在 nums 中的数
+//字，并以数组的形式返回结果。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [4,3,2,7,8,2,3,1]
+//输出：[5,6]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1,1]
+//输出：[2]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// n == nums.length 
+// 1 <= n <= 105 
+// 1 <= nums[i] <= n 
+// 
+//
+// 进阶：你能在不使用额外空间且时间复杂度为 O(n) 的情况下解决这个问题吗? 你可以假定返回的数组不算在额外空间内。 
+// Related Topics 数组 
+// 👍 751 👎 0
+
+
+package leetcode.editor.cn;
+
+//找到所有数组中消失的数字
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P448_FindAllNumbersDisappearedInAnArray {
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new P448_FindAllNumbersDisappearedInAnArray().new Solution();
+    }
+
+    //力扣代码
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<Integer> findDisappearedNumbers(int[] nums) {
+            int n = nums.length;
+            for (int num : nums) {
+                int x = (num - 1) % n;
+                nums[x] += n;
+            }
+            List<Integer> ret = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                if (nums[i] <= n) {
+                    ret.add(i + 1);
+                }
+            }
+            return ret;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
